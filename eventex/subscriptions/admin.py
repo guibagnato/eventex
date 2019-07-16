8ab1b1ec094b0dae08ba3cad52d6d8077f9b5dea
guibaganto <gui.bagnato@gmail.com>
2019-07-16 11:04:23 -0300

    Adiona campo paid

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -5,16 +5,16 @@ from eventex.subscriptions.models import Subscription
 
 
 class SubscriptionModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone', 'cpf', 'created_at', 'subscribed_today')
+    list_display = ('name', 'email', 'phone', 'cpf', 'created_at', 'subscribed_today', 'paid')
     date_hierarchy = 'created_at'
     search_fields = ('name', 'email', 'phone', 'cpf', 'created_at')
 
     def subscribed_today(self, obj):
-        return obj.created_at == now().date()
+        return obj.created_at.date() == now().date()
 
     subscribed_today.short_description = 'inscrito hoje ?'
     subscribed_today.boolean = True
-    list_filter = ('created_at',)
+    list_filter = ('paid', 'created_at')
 
 
 admin.site.register(Subscription, SubscriptionModelAdmin)

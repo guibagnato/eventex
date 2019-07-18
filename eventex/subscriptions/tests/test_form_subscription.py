6ed0b57e47dd0447f619843769104d3adae1ba49
guibaganto <gui.bagnato@gmail.com>
2019-07-18 10:33:32 -0300

    Email ou telefone opcionais

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -24,11 +24,26 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(name='GUILHERME BAGNATO')
         self.assertEqual('Guilherme Bagnato', form.cleaned_data['name'])
 
+    def test_email_is_optional(self):
+        """Email is optional"""
+        form = self.make_validated_form(email='')
+        self.assertFalse(form.errors)
+
+    def test_phone_is_optional(self):
+        """Phone is optional"""
+        form = self.make_validated_form(phone='')
+        self.assertFalse(form.errors)
+
     def assertFormErrorCode(self, form, field, code):
         errors = form.errors.as_data()
         errors_list = errors[field]
         exception = errors_list[0]
         self.assertEqual(code, exception.code)
+
+    def test_must_inform_email_or_phone(self):
+        """Email and Phone are optional, but one must be informed."""
+        form = self.make_validated_form(email='', phone='')
+        self.assertListEqual(['__all__'], list(form.errors))
 
     def assertFormErrorMessage(self, form, field, msg):
         errors = form.errors
--- eventex/subscriptions/tests/test_mail_subscribe.py
+++ eventex/subscriptions/tests/test_mail_subscribe.py
@@ -2,6 +2,7 @@ from django.core import mail
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
 
+
 class SubscribePostValid(TestCase):
     def setUp(self):
         data = dict(name='Guilherme Bagnato', cpf='12345678901',

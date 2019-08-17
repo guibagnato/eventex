5602bd97f1dc5c924c55382c8b2f9462ff99228e
guibaganto <gui.bagnato@gmail.com>
2019-08-17 15:55:32 -0300

    couse como mti

--- eventex/core/tests/test_view_talk_list.py
+++ eventex/core/tests/test_view_talk_list.py
@@ -1,5 +1,5 @@
-from django.test import TestCase
 from django.shortcuts import resolve_url as r
+from django.test import TestCase
 
 from eventex.core.models import Talk, Speaker, Course
 
@@ -13,9 +13,9 @@ class TalkListGet(TestCase):
                                  start='13:00',
                                  description='Descrição da palestra.')
         c1 = Course.objects.create(title='Título do Curso',
-                                   start='09:00',
-                                   description='Descrição do curso.',
-                                   slots=20)
+                                      start='09:00',
+                                      description='Descrição do curso.',
+                                      slots=20)
 
         speaker = Speaker.objects.create(
             name='Henrique Bastos',
@@ -54,7 +54,7 @@ class TalkListGet(TestCase):
                 self.assertContains(self.resp, expected, count)
 
     def test_context(self):
-        variables = ['morning_talks', 'afternoon_talks', 'courses']
+        variables = ['morning_talks', 'afternoon_talks']
 
         for key in variables:
             with self.subTest():
